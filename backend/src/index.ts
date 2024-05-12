a5c0e872de8ef4ed9c63793d0f3b52141442c7bf
emperorpenguin9 <flying.penguin9196@outlook.com>
2024-05-11 22:14:55 -0400

    new test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -43,7 +43,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
 
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../../frontend/dist"));
 });
 
 app.listen(7000, () => {
