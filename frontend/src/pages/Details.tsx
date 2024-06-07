c39efbdb5d01673b1ef19124047692f7772cc901
emperorpenguin9 <flying.penguin9196@outlook.com>
2024-06-07 15:43:32 -0400

    GuestInfoForm

--- frontend/src/pages/Details.tsx
+++ frontend/src/pages/Details.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import * as apiClient from "../api-client";
 import { AiFillStar } from "react-icons/ai";
+import GuestInfoForm from "./forms/GuestInfoForm/GuestInfoForm";
 
 const Detail = () => {
   const { hotelId } = useParams();
@@ -27,6 +28,36 @@ const Detail = () => {
           ))}
         </span>
         <h1 className="text-3xl font-bold">{hotel.name}</h1>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        {hotel.imageUrls.map((image, idx) => (
+          <div className="h-[300px]" key={idx}>
+            <img
+              src={image}
+              alt={hotel.name}
+              className="rounded-md w-full h-full object-cover object-center"
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-2">
+        {hotel.facilities.map((facility, idx) => (
+          <div className="border border-slate-200 rounded-sm p-3" key={idx}>
+            {facility}
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr]">
+        <div className="whitespace-pre-line">{hotel.description}</div>
+        <div className="h-fit">
+          <GuestInfoForm
+            pricePerNight={hotel.pricePerNight}
+            hotelId={hotel._id}
+          />
+        </div>
       </div>
     </div>
   );
